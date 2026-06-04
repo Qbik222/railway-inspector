1611efa4fc8f8eb926fd0d67165083cbba731279
Ihnatov Yurii <ignatovyw@gmail.com>
2026-06-04 08:53:38 +0300

    upd path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "node:url";
 import { dirname, resolve } from "node:path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
+    base: "./",
     plugins: [react()],
     resolve: {
         alias: {
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { dirname, resolve } from "node:path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
